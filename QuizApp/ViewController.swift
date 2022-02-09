f9285aef208dcd05fcca326d008e42d676c536e6
damlacim <cim.damla@gmail.com>
2022-02-09 22:24:47 +0300

    created main screen

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var buttonFalse: UIButton!
+    @IBOutlet weak var buttonTrue: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
