8e3155fe06438e6e673a7d66e87bdc194f3fe9db
damlacim <cim.damla@gmail.com>
2022-02-10 21:09:25 +0300

    add Questions Class

---
+++ QuizApp/Questions.swift
@@ -0,0 +1,18 @@
+//
+//  Questions.swift
+//  QuizApp
+//
+//  Created by Damla Çim on 10.02.2022.
+//
+
+import Foundation
+
+struct Quenstions {
+    
+    let text : String
+    let answer : String
+    
+    
+    
+    
+}
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -14,12 +14,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonTrue: UIButton!
     @IBOutlet weak var buttonFalse: UIButton!
     
+    let quiz = [
+        Quenstions(text: "Kotlin is the world's best programming language.", answer: "True"),
+        Quenstions(text: "Damla still has not found an internship.", answer: "True"),
+        Quenstions(text: "The most wonderful of sodas is Beypazarı.", answer: "True"),
+        Quenstions(text: "I couldn't find a question to write here. Press the False button.", answer: "False")
+        
+        
+        
+        
+    
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
         
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        
+        let userAnswer = sender.currentTitle //true ya da false
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        }
+        else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber].text
+        
     }
     
 
