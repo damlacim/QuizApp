df2b693f6e660a8d97b227ede6f6a2ac2f554e63
damlacim <cim.damla@gmail.com>
2022-03-05 15:11:33 +0300

    add network layer

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonC2: UIButton!
     @IBOutlet weak var buttonC3: UIButton!
     
-   var quizLogic = Logic()
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,38 +28,12 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         
-        guard let userAnswer = sender.currentTitle else {
-            return
-        }
-        
-        let userGotItRight = quizLogic.checkAnswer(userAnswer)
-        
-        if userGotItRight {
-            sender.backgroundColor = UIColor.green
-        }
-        else {
-            sender.backgroundColor = UIColor.red
-        }
-        
-        quizLogic.nextQuestion()
-        
-        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+       
         
     }
     
     @objc func updateUI() {
-        questionLabel.text = quizLogic.getQuestionText()
-        let answerChoices = quizLogic.getAnswer()
-        buttonC1.setTitle(answerChoices[0], for: .normal)
-        buttonC2.setTitle(answerChoices[1], for: .normal)
-        buttonC3.setTitle(answerChoices[2], for: .normal)
         
-        progressBar.progress = quizLogic.getProgress()
-        scoreLabel.text = "Score: \(quizLogic.getScore())"
-        
-        buttonC1.backgroundColor = UIColor.clear
-        buttonC2.backgroundColor = UIColor.clear
-        buttonC3.backgroundColor = UIColor.clear
         
         
     }
---
+++ QuizApp/Model/QuizManager.swift
@@ -0,0 +1,43 @@
+//
+//  QuizManager.swift
+//  QuizApp
+//
+//  Created by Damla Çim on 5.03.2022.
+//
+
+import Foundation
+
+struct QuizManager {
+    
+    let quizURL = "https://quizapi.io/api/v1/questions?apiKey=PgtZ92pOzQjokad7tqq89vwsisdqoxpjgHBugbnR&category=bash&limit=10"
+    
+    func fetchQuiz(url: String) {
+        performRequest(urlString: url )
+    }
+    
+    func performRequest(urlString: String) {
+        //Create a URL
+        if let url = URL(string: quizURL) {
+            //Create a URLSession
+            let session = URLSession(configuration: .default)
+            //Give URLSession a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:urlResponse:error:))
+            
+            //Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+        }
+    }
+    
+    
+    
+}
