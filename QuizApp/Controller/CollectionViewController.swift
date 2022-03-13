b64cf679d5a7b5947fb4175dfd9348c6d4de582c
damlacim <cim.damla@gmail.com>
2022-03-13 18:00:13 +0300

    add navigation controller

---
+++ QuizApp/Controller/CollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewController.swift
+//  QuizApp
+//
+//  Created by Damla Çim on 13.03.2022.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ QuizApp/View/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  QuizApp
+//
+//  Created by Damla Çim on 13.03.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
