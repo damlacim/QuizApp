8d098512d68bc017702163d92b1e22b1b6e25a13
damlacim <cim.damla@gmail.com>
2022-03-20 19:34:32 +0300

    show dummy score in table view cell

--- QuizApp/Controller/AchievementViewController.swift
+++ QuizApp/Controller/AchievementViewController.swift
@@ -7,25 +7,42 @@
 
 import UIKit
 
-class AchievementViewController: UIViewController {
+class AchievementViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
 
     @IBOutlet weak var table: UITableView!
     
     let data = UserDefaults.standard
     var scoreList: [Int]?
-
-   
     
+    var dummyScore: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        table.delegate = self
+        table.dataSource = self
+        
         data.set(scoreList, forKey: "new score") //score'u user defaults'a kaydettim.
         let scoreAchievementList = data.array(forKey: "new score") as? [Int] ?? [Int]() //score'ları arrayde tutucaz
 
+        dummyScore = ["Score: 1","Score: 3","Score: 5", "Score: 0","Score: 6", "Score: 2","Score: 5"]
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyScore.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        cell.textLabel?.text = dummyScore[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        return cell
+    }
     
 
   
